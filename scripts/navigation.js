d7fc52446a91c12975e6e0bdbbd2b7b6d4b93e76
Oniyemmy24 <Oniyemmy24@gmail.com>
2025-05-12 01:42:20 +0100

    navigation.js added

---
+++ scripts/navigation.js
@@ -0,0 +1,7 @@
+const menuButton = document.getElementById('menu');
+const navMenu = document.getElementById('navMenu');
+
+menuButton.addEventListener('click', () => {
+  navMenu.classList.toggle('open');
+  menuButton.classList.toggle('open');
+});
